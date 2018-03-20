58170be27dcc1994a0920b7286891cad929b36eb
difr <dialfr@gmail.com>
2018-03-20 20:10:48 +0300

    land: Use lineage vibrator

--- device.mk
+++ device.mk
@@ -407,8 +407,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.lineage
 
 # VNDK-SP
 PRODUCT_PACKAGES += \

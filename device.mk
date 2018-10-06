9f0d19ec1072dce7748f6c9c66b826aa3c38c125
difr <dialfr@gmail.com>
2018-10-07 02:24:04 +0300

    land: Kill power hal

--- device.mk
+++ device.mk
@@ -331,10 +331,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     $(LOCAL_PATH)/configs/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.land
-
 # Spectrum
 PRODUCT_PACKAGES += \
     Spectrum
